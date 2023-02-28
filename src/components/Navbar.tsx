2b7944ac9f6e3808b044482fe9b0d0ad2495a975
ginsum <immime12697@gmail.com>
2023-03-01 00:57:00 +0900

    FIX: Navbar 수정

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,19 +3,20 @@ import { Link, useLocation } from 'react-router-dom';
 const NavBar = () => {
   const { pathname } = useLocation();
   const isBookmark = pathname.includes('bookmark');
+
   const linkStyle = 'w-1/2 flex justify-center items-center';
-  const activeStyle = 'bg-zinc-100';
+  const activeStyle = 'text-blue-500 font-semibold border-b border-blue-300';
 
   return (
-    <div className="flex w-full h-12 text-zinc-400 text-sm border-b border-zinc-200">
+    <div className="flex w-full h-12 text-zinc-400 text-sm border-b border-zinc-200 mb-5">
       <Link to="/" className={`${linkStyle} ${isBookmark ? '' : activeStyle}`}>
-        <div className="">Search</div>
+        Search
       </Link>
       <Link
         to="/bookmark"
         className={`${linkStyle} ${isBookmark ? activeStyle : ''}`}
       >
-        <div className="">Bookmark</div>
+        Bookmark
       </Link>
     </div>
   );
