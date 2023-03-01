e10d14b9d347429a6fd1fd1add3435ab1437df8e
ginsum <immime12697@gmail.com>
2023-03-01 17:50:58 +0900

    FEAT: Skeleton 추가

---
+++ src/components/Skeleton.tsx
@@ -0,0 +1,16 @@
+const listArr = new Array(8).fill('1');
+
+const Skeleton = () => {
+  return (
+    <div className="mt-5">
+      {listArr.map((el, index) => (
+        <div
+          key={index}
+          className="animate-pulse w-full h-20 mb-3 bg-zinc-200 rounded-xl"
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Skeleton;
